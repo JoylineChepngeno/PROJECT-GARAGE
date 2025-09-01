5d2ed1ea1d8bfb27b46a4457fa77197d99edc250
Joyline <joylinechep001@gmail.com>
2025-09-01 22:54:26 +0300

    feat(app-routing): lazy load the garage and system-admin component

--- garage-frontend/src/app/app.routes.ts
+++ garage-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing/landing';
 import { Dashbboard } from './mechanic/dashbboard/dashbboard';
+import { Layout } from './garage-admin/layout/layout';
+import { Dashboard } from './system-admin/dashboard/dashboard';
 
 export const routes: Routes = [
     //landing page route
@@ -11,5 +13,12 @@ export const routes: Routes = [
             import('./car-owner/car-owner.routes').then(m=> m.Car_Owner_Routes), 
         },
     //lazy load mechanic component
-    { path: 'mechanic', component: Dashbboard}
+    { path: 'mechanic', component: Dashbboard},
+
+    //lazy load garage component
+    {path: 'garage-admin', component: Layout},
+
+    //lazy load system admin
+    {path: 'system-admin', component: Dashboard}
+
 ];
