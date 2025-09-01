68bd0729952d17972995bcfea7c44a6943c29750
Joyline <joylinechep001@gmail.com>
2025-09-01 21:43:57 +0300

    feat(app-routing): add lazy loading for car-owner routes

--- garage-frontend/src/app/app.routes.ts
+++ garage-frontend/src/app/app.routes.ts
@@ -3,5 +3,10 @@ import { Landing } from './landing/landing/landing';
 
 export const routes: Routes = [
     //landing page route
-    {path:'', component: Landing}
+    {path:'', component: Landing},
+    //lazy load the car-owner component
+    {path: 'car-owner',
+        loadChildren: () =>
+            import('./car-owner/car-owner.routes').then(m=> m.Car_Owner_Routes), 
+        },
 ];
