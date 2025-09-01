5b236420bd6d24a831fffa46d2b56d148cf878d0
Joyline <joylinechep001@gmail.com>
2025-09-01 17:13:29 +0300

    feat:set up landing page

---
+++ garage-frontend/src/app/landing/landing/landing.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { HeroSection } from "../hero-section/hero-section";
+import { Guide } from "../guide/guide";
+import { Footer } from "../footer/footer";
+
+@Component({
+  selector: 'app-landing',
+  imports: [Header, HeroSection, Guide, Footer],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css'
+})
+export class Landing {
+
+}
