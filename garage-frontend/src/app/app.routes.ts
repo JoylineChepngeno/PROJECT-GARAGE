9a4ebc49c94ef1dfbabf74aedd3928caedbc6d58
Joyline <joylinechep001@gmail.com>
2025-09-01 18:29:28 +0300

    feat: add route for landing page

--- garage-frontend/src/app/app.routes.ts
+++ garage-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Landing } from './landing/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //landing page route
+    {path:'', component: Landing}
+];
