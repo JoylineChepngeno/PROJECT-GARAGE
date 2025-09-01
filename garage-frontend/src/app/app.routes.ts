b842ca8e156beeac8bb1ac743a032063abfbccf1
Joyline <joylinechep001@gmail.com>
2025-09-01 22:18:32 +0300

    feat(app routing): set up mechanic routing

--- garage-frontend/src/app/app.routes.ts
+++ garage-frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Landing } from './landing/landing/landing';
+import { Dashbboard } from './mechanic/dashbboard/dashbboard';
 
 export const routes: Routes = [
     //landing page route
@@ -9,4 +10,6 @@ export const routes: Routes = [
         loadChildren: () =>
             import('./car-owner/car-owner.routes').then(m=> m.Car_Owner_Routes), 
         },
+    //lazy load mechanic component
+    { path: 'mechanic', component: Dashbboard}
 ];
