b9fb43f4e4597785ee8fcf53bdd01913465a7205
Joyline <joylinechep001@gmail.com>
2025-09-10 17:00:43 +0300

    feat: Add routers for navigation to register and login

--- garage-frontend/src/app/landing/guide/guide.ts
+++ garage-frontend/src/app/landing/guide/guide.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-guide',
   imports: [],
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './guide.css'
 })
 export class Guide {
+
+    //direct to register
+  constructor(private router: Router){}
+
+  goToRegister(){
+    this.router.navigate(['/register']);
+    
+  }
+
+  //direct to log in
+
+  goToLogIn(){
+    this.router.navigate(['/login'])
+  }
 
 }
