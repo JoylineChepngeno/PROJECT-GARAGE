9c5a74ead09e689bd7917f210ce49ceffe4ecff2
Joyline <joylinechep001@gmail.com>
2025-09-11 12:52:18 +0300

    style: add selectors and basic form styling

--- garage-frontend/src/app/garage-admin/garage-admin-registration/financial-info.ts
+++ garage-frontend/src/app/garage-admin/garage-admin-registration/financial-info.ts
@@ -1,23 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input} from '@angular/core';
-import { FormGroup, ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormGroup,  } from "@angular/forms";
+
 
 @Component({
   selector: 'app-financial-info',
-  imports: [ɵInternalFormsSharedModule],
+  imports: [CommonModule],
   template: `
-
+  <div class="form-field">
   <label>MPESA PayBill</label>
   <input type="text" formControlName="mpesaPayBill">
+  </div>
 
+  <div class="form-field">
   <label>MPESA Till</label>
   <input type="text" formControlName="mpesaTill">
+  <div>
   
   `,
+
+  styles: [`
+
+      .form-field{
+        display:flex;
+        flex-direction: column;
+        margin-bottom: 10px;
+
+      }
+
+      .form-field label{
+        margin-bottom: 5px;
+        font-weight: 400;
+      }
+
+    `]
+
  
 })
 export class FinancialInfo {
 
-  @Input() group!: FormGroup<any>
+  @Input() group!: FormGroup<any>;
 
 
 
