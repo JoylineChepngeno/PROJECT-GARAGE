ca10cfdcdd24c7a05241f30b5f82c2c0120bbede
Joyline <joylinechep001@gmail.com>
2025-09-11 12:57:50 +0300

    style: add selectors and basic form styling

--- garage-frontend/src/app/garage-admin/garage-admin-registration/operational-details.ts
+++ garage-frontend/src/app/garage-admin/garage-admin-registration/operational-details.ts
@@ -1,23 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormGroup, ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormGroup, } from "@angular/forms";
 
 @Component({
   selector: 'app-operational-details',
-  imports: [ɵInternalFormsSharedModule],
+  imports: [CommonModule],
   template: `
 
-  <label>Years in Operation</label>
-  <input type="text" formControlName="yearsInOperation" >
+  <div class="form-field">
+    <label>Years in Operation</label>
+    <input type="text" formControlName="yearsInOperation" >
+  </div>
 
-  <label>Operating Hours</label>
-  <input type="text" formControlName="operatingHours">
+  <div class="form-field">
+    <label>Operating Hours</label>
+    <input type="text" formControlName="operatingHours">
+  </div>
 
-  <label>24/7</label>
-  <input type="text" formControlName="twentyFourHours">
+  <div class="form-field">
+    <label>24/7</label>
+    <input type="text" formControlName="twentyFourHours">
+  </div>
 
   `,
+
+  styles: [`
+
+    .form-field {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 15px;
+    margin-top:15px;
+
+    }
+
+    .form-field label{
+      margin-bottom: 5px;
+      font-weight: 400;
+    }
+
+    .form-field input{
+      padding: 8px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      
+    }
+
+    `]
  
 })
+
 export class OperationalDetails {
   @Input() group!: FormGroup<any>
 
