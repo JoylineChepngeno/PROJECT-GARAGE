126b2b67fcf9474f9978fdb30d8f9d3672abbfc0
Joyline <joylinechep001@gmail.com>
2025-09-11 12:58:04 +0300

    style: add selectors and basic form styling

--- garage-frontend/src/app/garage-admin/garage-admin-registration/review-submit.ts
+++ garage-frontend/src/app/garage-admin/garage-admin-registration/review-submit.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-review-submit',
-  imports: [],
-  template: ``,
+  imports: [CommonModule],
+  template: `
+  <div class="form-label">
+  <label>Review</label>
+  <input type="text" formControlName="review">
+</div>
+  `,
+
+  styles:[`
+
+      .form-label{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 10px;
+      }
+
+      .form-label label{
+        font-weight: 400;
+        margin-bottom: 5px;
+      }
+
+      .form-label input{
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 8px;
+      }
+    `
+
+  ]
  
 })
 export class ReviewSubmit {
+  @Input() group!: FormGroup<any>
 
 }
