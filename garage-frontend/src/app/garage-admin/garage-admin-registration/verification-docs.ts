dc40095527b637f46a9948acd948ebe56fcf7ba3
Joyline <joylinechep001@gmail.com>
2025-09-11 12:58:26 +0300

    style: add selectors and basic form styling

--- garage-frontend/src/app/garage-admin/garage-admin-registration/verification-docs.ts
+++ garage-frontend/src/app/garage-admin/garage-admin-registration/verification-docs.ts
@@ -1,19 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-verification-docs',
-  imports: [],
+  imports: [CommonModule],
   template: `
 
-  <label>Business License </label>
+  <div class="form-label">
+    <label>Business License </label>
   <input type="file">
-  <label>Facility Photos</label>
+  </div>
+
+ <div class="form-label">
+   <label>Facility Photos</label>
   <input>
-  <label></label>
-  <input>
+ </div>
+  
+
   
   `,
+
+  styles: [`
+
+      .form-label{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 10px;
+      }
+
+      .form-label label{
+        font-weight: 400;
+        margin-bottom: 5px;
+      }
+
+      .form-label input{
+        padding: 5px;
+        border: 1px solid #353333ff;
+        border-radius: 4px;
+      }
+    `]
  
 })
 export class VerificationDocs {
