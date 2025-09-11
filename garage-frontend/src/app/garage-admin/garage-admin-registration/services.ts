33b7b7b25257d301327290f09fc4c1ac971b46cb
Joyline <joylinechep001@gmail.com>
2025-09-11 12:58:13 +0300

    style: add selectors and basic form styling

--- garage-frontend/src/app/garage-admin/garage-admin-registration/services.ts
+++ garage-frontend/src/app/garage-admin/garage-admin-registration/services.ts
@@ -1,21 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component,Input } from '@angular/core';
-import { FormGroup, ɵInternalFormsSharedModule } from "@angular/forms";
+import { FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-review-submit',
-  imports: [ɵInternalFormsSharedModule],
+  selector: 'app-services',
+  imports: [CommonModule],
   template: `
-
-  <label>Service Categories</label>
+<div class="form-label">
+    <label>Service Categories</label>
   <input formControlName="serviceCategories">
+</div>
 
-  <label> Specialised Services</label>
+  <div class="form-label">
+    <label> Specialised Services</label>
   <input formControlName="specialised Services">
   
   `,
+
+  styles:[`
+
+    .form-label{
+      display: flex;
+      flex-direction: columns;
+      margin-bottom: 10px;
+    }
+
+    .form-label-label{
+      font-weight: 400;
+      margin-bottom: 5px;
+    }
+
+    .form-label input{
+      padding: 8px;
+      border: 1px solid #0e0d0dff;
+      border-radius: 4px
+    }
+
+
+
+    `]
  
 })
-export class ReviewSubmit{
+export class Services{
   @Input() group! : FormGroup<any>
 
   
